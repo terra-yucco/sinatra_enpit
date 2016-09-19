8425dfdd1e6db468b60818d65382a2539c571a87
tryc <terashima.yuko@gmail.com>
2016-09-19 05:55:04 +0000

    Remove old contents, and refine erb.(index)

--- index.rb
+++ index.rb
@@ -1,24 +1,6 @@
 require 'sinatra'
 
 get '/' do
-  @title = 'Greet by English'
-  @greeting = 'Hello.'
+  @title = "terra-yucco's site"
   erb :index
-end
-
-get '/jp' do
-<<EOS
-<html>
-  <head>
-    <title>Greet by Japanese.</title>
-  </head>
-  <body>
-    <h1>おはようございます。</h1>
-  </body>
-</html>
-EOS
-end
-
-get '/user/:name' do
-  "Hello #{params['name']}!"
 end
